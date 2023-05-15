1dda6d3dcc7d76d4fd4414ec84938e95e6d924f6
42decibel <hyukzuny@gmail.com>
2023-05-15 18:48:33 +0900

    feat: Add cmd line control test

---
+++ basic/getopt-fucntion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    int n;
+    extern char *optarg;
+    extern int optind;
+
+    printf("current optind: %d\n", optind);
+
+    while ((n = getopt(argc, argv, "abc:")) != -1) {
+        switch (n)
+        {
+        case 'a':
+            printf("Option: a\n");
+            break;
+        case 'b':
+            printf("Option: b\n");
+            break;
+        case 'c':
+            printf("Option: c\n");
+            break;
+        default:
+            break;
+        }
+
+        printf("next optind: %d\n", optind);
+    }
+}
