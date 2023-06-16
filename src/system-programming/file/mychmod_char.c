69af9e4d12ec2c658c79c9f64728b5b5329431ac
42decibel <hyukzuny@gmail.com>
2023-06-16 16:00:29 +0900

    feat: Add file resources

--- src/system-programming/file/mychmod_char.c
+++ src/system-programming/file/mychmod_char.c
@@ -16,25 +16,189 @@ int main(int argc, char *argv[]) {
     }
 
     char *auth;
+    int auth_len = 0;
     char *file_name;
 
     auth = argv[1];
+    auth_len = strlen(auth);
     file_name = argv[2];
 
     struct stat statbuf;
 
-    chmod("jo.txt", S_IRWXG | S_IRGRP | S_IXGRP | S_IROTH);
-
-    stat("jo.txt", &statbuf);
+    stat(file_name, &statbuf);
 
     printf("mode = %o\n", (unsigned int)statbuf.st_mode);
 
-    statbuf.st_mode |= S_IWGRP;
-    statbuf.st_mode &= ~(S_IROTH);
+    if (auth_len < 3) {
+        printf("input auth correct\n");
 
-    chmod("jo.txt", statbuf.st_mode);
-    
-    stat("jo.txt", &statbuf);
+        return 0;
+    }
+
+    char accesser;
+    accesser = auth[0];
+
+    char appender;
+    appender = auth[1];
+
+    switch (accesser)
+    {
+    case 'u':
+        if (appender == '+') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                    statbuf.st_mode &= ~(S_IRUSR);
+                }
+                else if (auth[i] == 'w') {
+                    statbuf.st_mode &= ~(S_IWUSR);
+                }
+                else if (auth[i] == 'x') {
+                    statbuf.st_mode &= ~(S_IXUSR);
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else if (appender == '-') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                    statbuf.st_mode &= ~(S_IRUSR);
+                }
+                else if (auth[i] == 'w') {
+                    statbuf.st_mode &= ~(S_IWUSR);
+                }
+                else if (auth[i] == 'x') {
+                    statbuf.st_mode &= ~(S_IXUSR);
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else if (appender == '=') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                }
+                else if (auth[i] == 'w') {
+                }
+                else if (auth[i] == 'x') {
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else {
+            printf("invalid access\n");
+        }
+        break;
+    case 'g':
+        if (appender == '+') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                    statbuf.st_mode &= ~(S_IRGRP);
+                }
+                else if (auth[i] == 'w') {
+                    statbuf.st_mode &= ~(S_IWGRP);
+                }
+                else if (auth[i] == 'x') {
+                    statbuf.st_mode &= ~(S_IXGRP);
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else if (appender == '-') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                    statbuf.st_mode &= ~(S_IRGRP);
+                }
+                else if (auth[i] == 'w') {
+                    statbuf.st_mode &= ~(S_IWGRP);
+                }
+                else if (auth[i] == 'x') {
+                    statbuf.st_mode &= ~(S_IXGRP);
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else if (appender == '=') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                }
+                else if (auth[i] == 'w') {
+                }
+                else if (auth[i] == 'x') {
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else {
+            printf("invalid access\n");
+        }
+        break;
+    case 'o':
+        if (appender == '+') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                    statbuf.st_mode &= ~(S_IROTH);
+                }
+                else if (auth[i] == 'w') {
+                    statbuf.st_mode &= ~(S_IWOTH);
+                }
+                else if (auth[i] == 'x') {
+                    statbuf.st_mode &= ~(S_IXOTH);
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else if (appender == '-') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                    statbuf.st_mode &= ~(S_IROTH);
+                }
+                else if (auth[i] == 'w') {
+                    statbuf.st_mode &= ~(S_IWOTH);
+                }
+                else if (auth[i] == 'x') {
+                    statbuf.st_mode &= ~(S_IXOTH);
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else if (appender == '=') {
+            for (int i = 2; i < auth_len; i++) {
+                if (auth[i] == 'r') {
+                }
+                else if (auth[i] == 'w') {
+                }
+                else if (auth[i] == 'x') {
+                }
+                else {
+                    printf("invalid access\n");
+                }
+            }
+        }
+        else {
+            printf("invalid access\n");
+        }
+        break;
+    default:
+        printf("invalid access\n");
+        break;
+    }
+
+    stat(file_name, &statbuf);
 
     printf("mode = %o\n", (unsigned int)statbuf.st_mode);
 }
