909befe8141bdb0dcdc520628974c6178e1b8978
42decibel <hyukzuny@gmail.com>
2023-06-25 15:38:48 +0900

    feat: Add file-io resources

---
+++ src/system-programming/04-file-io/file_fwrite.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main() {
+    FILE *rfp, *wfp;
+    char buf[BUFSIZ];
+    int c;
+
+    if ((rfp = fopen("jo.txt", "r")) == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+
+    if ((wfp = fopen("jo.bak", "r")) == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+
+    while ((c = fread(buf, sizeof(char)*2, 4,rfp)) > 0) {
+        fwrite(buf, sizeof(char)*2, c, wfp);
+    }
+
+    fclose(rfp);
+    fclose(wfp);
+}
