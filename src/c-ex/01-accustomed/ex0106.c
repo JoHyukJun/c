5bce5c2e43d9e2b639c9af994d9e62ce8b68607c
42decibel <hyukzuny@gmail.com>
2023-09-29 18:32:47 +0900

    feat: Add ces rsc

---
+++ src/c-ex/01-accustomed/ex0106.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int x;
+    x = 16;
+
+    printf("x=%d\n", x);
+
+    return 0;
+}
---
+++ src/c-ex/01-accustomed/ex0107.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int vx = 3.14;
+    int vy = 5.7;
+
+    printf("vx=%d\n", vx);
+    printf("vy=%d\n", vy);
+
+    return 0;
+}
