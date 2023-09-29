79f89824ae9f8ae77b633c498662a7980f685779
42decibel <hyukzuny@gmail.com>
2023-09-29 17:59:46 +0900

    feat: Add cex rsc

---
+++ src/c-ex/01-accustomed/ex0104.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void) {
+    printf("hi\n\nhi\nhello\n");
+
+    return 0;
+}
---
+++ src/c-ex/01-accustomed/ex0105.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void) {
+    printf("alert\aalert\a\n");
+
+    return 0;
+}
