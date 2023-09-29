4afb1badaa4747bbd5ff0dcb7628b46d04981f0b
42decibel <hyukzuny@gmail.com>
2023-09-29 13:26:28 +0900

    feat: Init c-ex dir

---
+++ src/c-ex/01-accustomed/ex0102.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/*
+    - printf 함수에 첫 번째 인수는 format string 이라고 함
+    - format string 안의 %d 는 형식을 지시하는 conversion specification 이라 함
+
+*/
+int main(void) {
+    printf("%d\n", 11 - 38);
+
+    return 0;
+}
