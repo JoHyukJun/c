34d20117f6edc5f7df29997892cdccd37f7eead1
42decibel <hyukzuny@gmail.com>
2023-10-22 16:12:27 +0900

    feat: Add cex rsc

---
+++ src/c-ex/01-accustomed/pr0112.c
@@ -0,0 +1,16 @@
+/*
+    - 다음은 "금수강산" 각 문자를 한 줄에 한 자씩 밀어서 표시하는 프로그램이다.
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    puts("금");
+    puts("수");
+    puts("강");
+    puts("산");
+
+    return 0;
+}
