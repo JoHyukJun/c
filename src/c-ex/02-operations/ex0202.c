eb27ce77eac83fa7ca041d0ff79528ddb3651d94
42decibel <hyukzuny@gmail.com>
2023-11-11 13:25:56 +0900

    feat: Add cex rsc

---
+++ src/c-ex/02-operations/ex0202.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int n;
+
+    printf("input integer:");
+    scanf("%d", &n);
+
+    printf("n / 5 = %d\n", n / 5);
+    printf("N %% 5 = %d\n", n %d);
+
+    return 0;
+}
