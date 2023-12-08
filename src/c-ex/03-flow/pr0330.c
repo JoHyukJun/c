8e10173e988619a589f2a28fceb1edc8469c80da
42decibel <hyukzuny@gmail.com>
2023-12-08 20:38:21 +0900

    feat: Add cex rsc

---
+++ src/c-ex/03-flow/pr0330.c
@@ -0,0 +1,13 @@
+/*
+    - if 문과 switch 문의 총칭은 (1) 문이다. 또한 이들 구문은 (2) 식을 평가한 결과를 기준으로 하여 프로그램 흐름을 제어한다.
+    - (1): 선택문(selection statement)
+    - (2): 제어
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    return 0;
+}
---
+++ src/c-ex/03-flow/pr0331.c
@@ -0,0 +1,12 @@
+/*
+    - 연산자 && 와 || 의 총칭은 (1) 연산자다.
+    - (1): 논리
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    return 0;
+}
