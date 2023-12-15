7c3e42c29f670c195eae255ea38a0c1d2d83a8dd
42decibel <hyukzuny@gmail.com>
2023-12-15 21:17:15 +0900

    feat: Add cex rsc

---
+++ src/c-ex/04-loop/pr0413.c
@@ -0,0 +1,35 @@
+/*
+    - 다음에 나타난 이름이 식별자로서 구문적으로 올바른지 O X 로 답하라.
+    - _a
+        - O
+    - $ab
+        - X
+    - ab_c
+        - O
+    - pc9821
+        - O
+    - while
+        - X
+    - While
+        - O
+    - BMW740Li
+        - O
+    - 007
+        - X
+    - ___a
+        - O
+    - ___1
+        - O
+    - a___
+        - O
+    - 1___
+        - X
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    return 0;
+}
---
+++ src/c-ex/04-loop/pr0414.c
@@ -0,0 +1,17 @@
+/*
+    - 다음 프로그램 부분의 실행 결과를 나타내라.
+    -
+        0
+        1
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    printf("%d\n", !1);
+    printf("%d\n", !0);
+
+    return 0;
+}
