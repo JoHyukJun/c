852bc73052f3037139e4bbbb9831500bd86385b8
42decibel <hyukzuny@gmail.com>
2023-12-16 13:52:30 +0900

    feat: Add cex rsc

--- src/c-ex/05-array/ex0502.c
+++ src/c-ex/05-array/ex0502.c
@@ -6,10 +6,10 @@ int main(void) {
     int v[5];
 
     for (i = 0; i < 5; i++)
-        v[i] = i;
+        v[i] = 5 - i;
 
     for (i = 0; i < 5; i++)
         printf("v[%d] = %d\n", i, v[i]);
-        
+
     return 0;
 }
--- src/c-ex/05-array/ex0503.c
+++ src/c-ex/05-array/ex0503.c
@@ -3,13 +3,10 @@
 
 int main(void) {
     int i;
-    int v[5];
-
-    for (i = 0; i < 5; i++)
-        v[i] = i;
+    int v[5] = {5, 4, 3, 2, 1};
 
     for (i = 0; i < 5; i++)
         printf("v[%d] = %d\n", i, v[i]);
-        
+
     return 0;
 }
