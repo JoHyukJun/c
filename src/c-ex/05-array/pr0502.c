c623cc4f9edb18a912b848fe6d1b3e75012cdad6
42decibel <hyukzuny@gmail.com>
2023-12-16 15:19:46 +0900

    feat: Add cex rsc

---
+++ src/c-ex/05-array/pr0502.c
@@ -0,0 +1,15 @@
+/*
+    - 다음은 뒤의 ABC 를 10 으로 치환하기 위한 정의다. ABC 는 (1) 이며 이를 (2) 이라고 한다.
+    - (1): 객체 형식 매크로(object-like macro)
+    - (2): 매크로명(macro name)
+*/
+
+
+#include <stdio.h>
+
+#define ABC 10
+
+
+int main(void) {
+    return 0;
+}
