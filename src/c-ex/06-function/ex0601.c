63248ad6ad696381b89c7e51895a42850bcc2a09
42decibel <hyukzuny@gmail.com>
2023-12-24 13:17:30 +0900

    feat: Add cex rsc

--- src/c-ex/06-function/ex0601.c
+++ src/c-ex/06-function/ex0601.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 
+int min2(int a, int b) {
+    return (a < b) ? a : b;
+}
+
 int main(void) {
+    int n1, n2;
+
+    puts("input 2 number.");
+    printf("n1: "); scanf("%d", &n1);
+    printf("n2: "); scanf("%d", &n2);
+
+    printf("min val: %d\n", min2(n1, n2));
+
     return 0;
 }
