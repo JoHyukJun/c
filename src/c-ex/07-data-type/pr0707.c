9c92a0f5a1a828b51891b72585de8196b91e165a
42decibel <hyukzuny@gmail.com>
2023-12-29 21:43:54 +0900

    feat: Add cex rsc

---
+++ src/c-ex/07-data-type/pr0707.c
@@ -0,0 +1,24 @@
+/*
+    - 각 정수형으로 표현할 수 있는 최솟값과 최댓값은 다음과 같은 이름으로 <(1)> 헤더 안에 정의돼 있다.
+    - char 형의 최솟값: (2)
+    - char 형의 최댓값: (3)
+    - int 형의 최솟값: (4)
+    - int 형의 최댓값: (5)
+    - unsigned char 형의 최댓값: (6)
+    - unsigned int 형의 최댓값: (7)
+    - (1): limits.h
+    - (2): CHAR_MIN
+    - (3): CHAR_MAX
+    - (4): INT_MIN
+    - (5): INT_MAX
+    - (6): UCHAR_MAX
+    - (7): UINT_MAX
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    return 0;
+}
---
+++ src/c-ex/07-data-type/pr0708.c
@@ -0,0 +1,13 @@
+/*
+    - 배열 v 의 요소 개수, 요소 자료형에 상관없이 그 요소 개수는 (1) / (2) 의 나눗셈으로 구할 수 있다.
+    - (1): sizeof(v)
+    - (2): sizeof(v[0])
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    return 0;
+}
