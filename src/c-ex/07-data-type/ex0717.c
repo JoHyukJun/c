24c8279e71151c2c7730c41e61761e8fffb00c96
42decibel <hyukzuny@gmail.com>
2023-12-29 19:32:49 +0900

    feat: Add cex rsc

--- src/c-ex/07-data-type/ex0717.c
+++ src/c-ex/07-data-type/ex0717.c
@@ -2,6 +2,20 @@
 
 
 int main(void) {
+    float x, sum = 0.0;
+
+    for (x = 0.0; x <= 1.0; x += 0.01)
+        sum += x;
+
+    printf("sum: %f\n", sum);
+
+    int i;
+    sum = 0.0;
+
+    for (i = 0; i <= 100; i++)
+        sum += i / 100.0;
+
+    printf("sum: %f\n", sum);
 
     return 0;
 }
