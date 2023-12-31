59037ffed106338a48de7d71965103987d33b0c0
42decibel <hyukzuny@gmail.com>
2023-12-31 09:32:25 +0900

    feat: Add cex rsc

--- src/c-ex/08-program/ex0801.c
+++ src/c-ex/08-program/ex0801.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+#define diff(x, y) (((x) > (y)) ? ((x) - (y)) : ((y) - (x)))
+
 
 int main(void) {
+    int a, b;
+
+    puts("2개의 정수를 입력하세요.");
+    printf("a: "); scanf("%d", &a);
+    printf("b: "); scanf("%d", &b);
+    printf("diff: %d\n", diff(a, b));
 
     return 0;
 }
---
+++ src/c-ex/README.md
@@ -0,0 +1,27 @@
+# c-ex
+
+## 01 accustomed
+
+## 02 operations
+
+## 03 flow
+
+## 04 loop
+
+## 05 array
+
+## 06 function
+
+## 07 data type
+
+## 08 program
+
+## 09 char
+
+## 10 pointer
+
+## 11 char pointer
+
+## 12 struct
+
+## 13 file
