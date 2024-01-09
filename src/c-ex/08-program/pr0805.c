5d7b433d4a070c6ed9cdb082f55b520bcd23cd10
42decibel <hyukzuny@gmail.com>
2024-01-09 20:57:34 +0900

    feat: Add cex rsc

---
+++ src/c-ex/08-program/pr0805.c
@@ -0,0 +1,16 @@
+/*
+    - 다음은 x 의 2 제곱과 3 제곱을 구하는 함수 형식 매크로다.
+*/
+
+
+#include <stdio.h>
+
+#define sqr(x) (x * x)
+#define cube(x) (x * x * x)
+
+int main(void) {
+    printf("sqr(3): %d\n", sqr(3));
+    printf("cube(3): %d\n", cube(3));
+
+    return 0;
+}
