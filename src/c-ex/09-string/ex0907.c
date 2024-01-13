0984a2a9cac0a5ffd32a0084df7731786557fb16
42decibel <hyukzuny@gmail.com>
2024-01-13 19:41:19 +0900

    feat: Add cex rsc

---
+++ src/c-ex/09-string/ex0907.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int str_chnum(const char s[], int c) {
+    int i;
+    int count = 0;
+
+    for (i = 0; s[i] != '\0'; i++)
+        if (s[i] == c)
+            count++;
+
+    return count;
+}
+
+int main(void) {
+    char str[256], ch[256];
+
+    printf("문자열 str 을 입력해주세요: ");
+    scanf("%s", str);
+
+    printf("문자 ch 을 입력해주세요: ");
+    scanf("%s", ch);
+
+    printf("str 은 ch 를 %d 개 포함합니다.\n", str_chnum(str, ch[0]));
+
+    return 0;
+}
