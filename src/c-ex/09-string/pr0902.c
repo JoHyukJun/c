829f663af1867a122a00ed7882f17b62446e9da6
42decibel <hyukzuny@gmail.com>
2024-01-14 19:31:33 +0900

    feat: Add cex rsc

---
+++ src/c-ex/09-string/pr0902.c
@@ -0,0 +1,16 @@
+/*
+    - 다음 프로그램 일부의 실행 결과를 나타내라.
+    ->
+        문자열 str = 740
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    char str[] = "740\0Li";
+    printf("문자열 str = %s\n", str);
+
+    return 0;
+}
