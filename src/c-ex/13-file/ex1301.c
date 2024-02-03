e42475a2cdb2dddd8725c2b66eeefbcd550e9f45
42decibel <hyukzuny@gmail.com>
2024-02-03 18:07:14 +0900

    feat: Add cex rsc

--- src/c-ex/13-file/ex1301.c
+++ src/c-ex/13-file/ex1301.c
@@ -2,6 +2,21 @@
 
 
 int main(void) {
+    FILE *fp;
+    char fname[256];
+
+    printf("파일 이름: ");
+    scanf("%s", fname);
+
+    fp = fopen(fname, "r");
+
+    if (fp == NULL) {
+        printf("이 파일은 없습니다.\n");
+    }
+    else {
+        printf("이 파일은 있습니다.\n");
+        fclose(fp);
+    }
 
     return 0;
 }
