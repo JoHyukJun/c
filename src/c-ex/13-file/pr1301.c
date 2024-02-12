5524cf445d1fced59af5b8f3032c8e5579bdc941
42decibel <hyukzuny@gmail.com>
2024-02-12 21:56:41 +0900

    feat: Add cex rsc

--- src/c-ex/13-file/pr1301.c
+++ src/c-ex/13-file/pr1301.c
@@ -1,5 +1,10 @@
 /*
-    - 
+    - C 언어 프로그램에서는 (1), (2), (3) 이라는 3가지 표준 스트림이 준비돼 있다.
+    - 이들 스트림을 가리키는 포인터는 (4) 헤더 안에 정의돼 있으며 각각의 이름은 stdin, stdout, stderr 이다.
+    - (1): 표준 입력 스트림(standard input stream)
+    - (2): 표준 출력 스트림(standard output steam)
+    - (3): 표준 오류 스트림(standard error stream)
+    - (4): stdio.h
 */
 
 
