ea8ac3ca760305713d9cde54447f45391ff48b75
42decibel <hyukzuny@gmail.com>
2024-12-06 18:04:05 +0900

    feat: Add jflib

---
+++ src/jlib/jflib/jflib.c
@@ -0,0 +1,6 @@
+#include "jflib.h"
+
+int main()
+{
+    printf("TEST\n");
+}
---
+++ src/jlib/jflib/jflib.h
@@ -0,0 +1,12 @@
+#ifndef __JFILIB__
+#define __JFILIB__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#endif
