a4de5ac506c59842f6adab98d0c744bf8134c11e
42decibel <hyukzuny@gmail.com>
2024-12-16 20:13:56 +0900

    feat: Add libj rsc

--- src/libj/samlib/samlib.c
+++ src/libj/samlib/samlib.c
@@ -8,7 +8,41 @@ int print_samlib()
     return 0;
 }
 
-int main()
+int shm_open(key)
+key_t key;
 {
-    printf("TEST\n");
+    int shmid;
+
+    shmid = shmget(key, SHM_SET_LEN, IPC_CREAT | 0644);
+
+    if (shmid == -1)
+    {
+        perror("shmget()");
+
+        return (-1);
+    }
+
+    return (shmid);
+}
+
+int shm_create(key)
+key_t key;
+{
+    int shmid;
+
+    shmid = shmget(key, SHM_SET_LEN, IPC_CREAT | IPC_EXCL | 0644);
+
+    if (shmid == -1 && errno == EEXIST)
+    {
+        shmid = shmget(key, SHM_SET_LEN, 0);
+
+        if (shmid == -1)
+        {
+            perror("shmget()");
+
+            return (-1);
+        }
+    }
+
+    return (shmid);
 }
