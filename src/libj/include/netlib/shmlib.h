90df20644f747648c09c7c9d88a2266267ab3a43
42decibel <hyukzuny@gmail.com>
2024-12-17 20:30:47 +0900

    feat: Add libj rsc

---
+++ src/libj/include/netlib/shmlib.h
@@ -0,0 +1,14 @@
+#ifndef __NETLIB__
+#define __NETLIB__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+
+int print_netlib();
+
+#endif
---
+++ src/libj/shmlib/shmlib.c
@@ -0,0 +1,117 @@
+#include "shmlib.h"
+
+
+int print_samlib()
+{
+    printf("SAMLIB\n");
+
+    return 0;
+}
+
+int shm_open(key)
+key_t key;
+{
+    int shmid;
+
+    shmid = shmget(key, SHM_SET_LEN, IPC_CREAT | 0644);
+
+    if (shmid == -1)
+    {
+        perror("shmget()");
+
+        return (-1);
+    }
+
+    return (shmid);
+}
+
+int shm_create(key)
+key_t key;
+{
+    int shmid;
+
+    shmid = shmget(key, SHM_SET_LEN, IPC_CREAT | IPC_EXCL | 0644);
+
+    if (shmid == -1 && errno == EEXIST)
+    {
+        shmid = shmget(key, SHM_SET_LEN, 0);
+
+        if (shmid == -1)
+        {
+            perror("shmget()");
+
+            return (-1);
+        }
+    }
+
+    return (shmid);
+}
+
+int shm_delete(shmid)
+int shmid;
+{
+    if (shmctl(shmid, 0, IPC_RMID) == -1)
+    {
+        perror("shmctl()");
+
+        return (-1);
+    }
+
+    return (0);
+}
+
+int sam_write(shmid, buf, buf_size)
+int shmid;
+char *buf;
+int buf_size;
+{
+    char *shmaddr;
+
+    shmaddr = shmat(shmid, (char *)0, 0);
+
+    if (shmaddr == (char *)-1)
+    {
+        perror("shmat()");
+
+        return (-1);
+    }
+
+    memcpy(shmaddr, buf, buf_size);
+
+    if (shmdt(shmaddr) == -1)
+    {
+        perror("shmdt()");
+
+        return (-1);
+    }
+
+    return (0);
+}
+
+int sam_read(shmid, buf, buf_size)
+int shmid;
+char *buf;
+int buf_size;
+{
+    char *shmaddr;
+
+    shmaddr = shmat(shmid, (char *)0, 0);
+
+    if (shmaddr == (char *)-1)
+    {
+        perror("shmat()");
+
+        return (-1);
+    }
+
+    memcpy(buf, shmaddr, buf_size);
+
+    if (shmdt(shmaddr) == -1)
+    {
+        perror("shmdt()");
+
+        return (-1);
+    }
+
+    return (0);
+}
